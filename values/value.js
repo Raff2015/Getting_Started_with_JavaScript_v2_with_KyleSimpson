994a54a378bd693a4e79f7cff6035bd15fe1be2c
Edgar Chicas <erc7@njit.edu>
2020-05-11 21:18:25 -0400

    js

--- values/value.js
+++ values/value.js
@@ -1,3 +1,14 @@
 var friends = ["Edgar", "Chicas"];
 console.log(friends[0]);
 console.log(friends.length);
+
+var age = 28;
+
+age++; // increment age add 1 but also reassign value back to age
+age += 2; //Starts from 0 1 2 = 3 which increments age +3. 28 + 3 = 31
+console.log(age);
+
+var edgarage = 31;
+
+edgarage = 1 + edgarage * 2;
+console.log(edgarage); //ANSWER 31 *2 +1 = 63
