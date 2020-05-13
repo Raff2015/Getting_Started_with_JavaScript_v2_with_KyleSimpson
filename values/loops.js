d2394f176022dd0e2008815c7357df3284fc3973
Edgar Chicas <erc7@njit.edu>
2020-05-12 20:45:07 -0400

    loops

---
+++ values/loops.js
@@ -0,0 +1,18 @@
+var students = [ /*..*/ ];
+
+for (let i = 0; i < students.length; i++) {
+    greetStudent(students[i]);
+}
+
+for (let student of students) {
+    greetStudent(student);
+}
+
+//console.log(students);
+
+var students = [ /*..*/ ];
+
+while (students.length > 0) {
+    let student = students.pop(); //pop() method that takes a value out
+    greetStudents(student);
+}
