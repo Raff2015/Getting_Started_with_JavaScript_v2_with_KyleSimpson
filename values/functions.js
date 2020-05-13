1170e06941ebc0c2d26fd4adbcd76bceadf669fa
Edgar Chicas <erc7@njit.edu>
2020-05-12 21:30:50 -0400

    functions

---
+++ values/functions.js
@@ -0,0 +1,13 @@
+/*function greetStudent(student) {
+    console.log(
+        `Hello, ${student.name}!`
+    );
+}*/
+
+function timeRemaining(timeElapsed, endTime) {
+    return (endTime - timeElapsed) / 2;
+}
+
+var left = timeRemaining(42, 140);
+
+console.log(left);
