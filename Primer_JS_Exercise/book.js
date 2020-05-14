0bf4af800f87150fe5b55efc282887786c3fe3b9
Edgar Chicas <erc7@njit.edu>
2020-05-13 21:09:59 -0400

    Primer

---
+++ Primer_JS_Exercise/book.js
@@ -0,0 +1,24 @@
+/* # Programming Primer (in JS)
+This is an exercise to practice basic programming principles.
+## Instructions
+
+The code of this exercise can be executed via Node.js or 
+or in the console tab of your browser's dev tools.
+
+1. Define an `addFavoriteBook(...)` function that receives
+a single parameter, called `bookName`.
+
+2. If the provided `bookName` string does *NOT*
+have the word "Great" in it, add it the `favoriteBooks` arrays.
+
+    Hints :
+    - `someString.includes(anotherString)` will return `true`
+    if `anotherString` is found inside `someString`, or `false`
+    */
+
+function addFavoriteBook(bookName) {
+
+    if (bookName = "Great") {
+
+    }
+}
