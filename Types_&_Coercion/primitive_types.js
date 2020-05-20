a38d802caddcbb2bf3a3c8496c7e7b1b876b02e4
Edgar Chicas <erc7@njit.edu>
2020-05-19 20:51:27 -0400

    typeof

---
+++ Types_&_Coercion/primitive_types.js
@@ -0,0 +1,17 @@
+var v;
+console.log(typeof v);
+
+v = "1";
+console.log(typeof v);
+
+v = 2;
+console.log(typeof v);
+
+v = true;
+console.log(typeof v);
+
+v = {};
+console.log(typeof v);
+
+v = Symbol();
+console.log(typeof v);
