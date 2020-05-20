7f7812f3317d318490d0a1205750f80adcedc168
Edgar Chicas <erc7@njit.edu>
2020-05-20 19:54:31 -0400

    boolean examples

---
+++ Types_&_Coercion/boolean.js
@@ -0,0 +1,9 @@
+var myFalse = new Boolean(false); // initial value of false
+var g = Boolean(myFalse); // initial value of true
+var myString = new String('Hello'); // string object
+var s = Boolean(myString); // initial value of true
+
+console.log(`var myFalse = new Boolean(false); ` + `ANSWER: ` + myFalse);
+console.log(`var g = Boolean(myFalse); ` + `ANSWER: ` + g);
+console.log(`var myString = new String('Hello'); ` + `ANSWER: ` + myString);
+console.log(`var s = Boolean(myString); ` + `ANSWER: ` + s);
