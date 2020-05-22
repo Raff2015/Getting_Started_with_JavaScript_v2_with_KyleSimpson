45b7a9818bad658cbd3b5eb84f1a7be415e45c0d
Edgar Chicas <erc7@njit.edu>
2020-05-21 21:10:49 -0400

    update

--- Types_&_Coercion/equality.js
+++ Types_&_Coercion/equality.js
@@ -9,8 +9,14 @@ var studentName2 = `${studentName1}`; // No Coercion for line 6 and 7
 var workshopEnrollment1 = 16;
 var workshopEnrollment2 = workshopEnrollment1 + 0;
 
-studentName1 == studentName2;
-studentName1 === studentName2;
+studentName1 == studentName2; // True because types are already the same (String)
+console.log(studentName1 == studentName2);
 
-workshopEnrollment1 == workshopEnrollment2;
-workshopEnrollment1 === workshopEnrollment2;
+studentName1 === studentName2; // True because types are already the same (String)
+console.log(studentName1 === studentName2);
+
+workshopEnrollment1 == workshopEnrollment2; // True because types are already the same (Number)
+console.log(workshopEnrollment1 == workshopEnrollment2);
+
+workshopEnrollment1 === workshopEnrollment2; // True because types are already the same  (Number)
+console.log(workshopEnrollment1 === workshopEnrollment2);
