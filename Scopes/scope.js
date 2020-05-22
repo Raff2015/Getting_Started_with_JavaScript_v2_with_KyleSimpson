e8cb673013de92c54a47137926d3254b73773b99
Edgar Chicas <erc7@njit.edu>
2020-05-21 21:54:03 -0400

    local_global

---
+++ Scopes/scope.js
@@ -0,0 +1,29 @@
+/*
+Local Variables
+*/
+// code here can NOT use carName
+
+function myFunction() {
+    var carName = "Volvo";
+
+    // code here CAN use carName
+
+}
+/*End of Local Variable*/
+
+//------------------------------------------------------
+/*
+Global Variable
+*/
+var carName = "Volvo";
+
+// code here can use carName
+
+function myFunction() {
+
+    // code here can also use carName
+
+}
+/*
+End of Global Variable
+*/
